ad4a1d38645b9143b09c960238fc8a3bfafec603
Ivan Koryakovskiy <i.koryakovskiy@tudelft.nl>
2018-01-11 14:17:41 +0100

    Icreasing penalty because robots start making leg-thigh contacts with the ground. For the came reason fail, if such contacts established

--- roboschool/grl_mujoco_walkers.py
+++ roboschool/grl_mujoco_walkers.py
@@ -20,31 +20,31 @@ class RoboschoolForwardWalkerMujocoXMLGRL(RoboschoolForwardWalker, RoboschoolMuj
         RoboschoolForwardWalker.__init__(self, power)
 
 class RoboschoolHopperGRL(RoboschoolForwardWalkerMujocoXMLGRL):
-    foot_list = ["foot"]
+    foot_list = ["foot", "thigh"] # diamiter of thigh is larger then leg, hence leg does not tocun the ground
     def __init__(self):
         RoboschoolForwardWalkerMujocoXMLGRL.__init__(self, "hopper.xml", "torso", action_dim=3, obs_dim=15, power=0.75)
     def alive_bonus(self, z, pitch):
-        alive = self.rwAlive if z > 0.6 and abs(pitch) < 1.5 else self.rwFail
+        alive = self.rwAlive if z > 0.6 and abs(pitch) < 1.5 and not self.feet_contact[1] else self.rwFail
         sick = 0 if z > 0.8 and abs(pitch) < 1.0 else 1
         return (alive, sick)
 
 class RoboschoolHopperBalancingGRL(RoboschoolForwardWalkerMujocoXMLGRL):
-    foot_list = ["foot"]
+    foot_list = ["foot", "thigh"]
     def __init__(self):
         RoboschoolForwardWalkerMujocoXMLGRL.__init__(self, "hopper.xml", "torso", action_dim=3, obs_dim=15, power=0.75)
         self.rwForward = 0
         self.rwTime = 0
     def alive_bonus(self, z, pitch):
-        alive = self.rwAlive if z > 0.6 and abs(pitch) < 1.5 else self.rwFail
+        alive = self.rwAlive if z > 0.6 and abs(pitch) < 1.5 and not self.feet_contact[1] else self.rwFail
         sick = 0 if z > 0.8 and abs(pitch) < 1.0 else 1
         return (alive, sick)
 
 class RoboschoolWalker2dGRL(RoboschoolForwardWalkerMujocoXMLGRL):
-    foot_list = ["foot", "foot_left"]
+    foot_list = ["foot", "foot_left", "thigh", "thigh_left"]
     def __init__(self):
         RoboschoolForwardWalkerMujocoXMLGRL.__init__(self, "walker2d.xml", "torso", action_dim=6, obs_dim=22, power=0.40)
     def alive_bonus(self, z, pitch):
-        alive = self.rwAlive if z > 0.6 and abs(pitch) < 1.5 else self.rwFail
+        alive = self.rwAlive if z > 0.6 and abs(pitch) < 1.5 and not self.feet_contact[2] and not self.feet_contact[3] else self.rwFail
         sick = 0 if z > 0.8 and abs(pitch) < 1.0 else 1
         return (alive, sick)
     def robot_specific_reset(self):
@@ -53,13 +53,13 @@ class RoboschoolWalker2dGRL(RoboschoolForwardWalkerMujocoXMLGRL):
             self.jdict[n].power_coef = 30.0
 
 class RoboschoolWalker2dBalancingGRL(RoboschoolForwardWalkerMujocoXMLGRL):
-    foot_list = ["foot", "foot_left"]
+    foot_list = ["foot", "foot_left", "thigh", "thigh_left"]
     def __init__(self):
         RoboschoolForwardWalkerMujocoXMLGRL.__init__(self, "walker2d.xml", "torso", action_dim=6, obs_dim=22, power=0.40)
         self.rwForward = 0
         self.rwTime = 0
     def alive_bonus(self, z, pitch):
-        alive = self.rwAlive if z > 0.6 and abs(pitch) < 1.5 else self.rwFail
+        alive = self.rwAlive if z > 0.6 and abs(pitch) < 1.5 and not self.feet_contact[2] and not self.feet_contact[3] else self.rwFail
         sick = 0 if z > 0.8 and abs(pitch) < 1.0 else 1
         return (alive, sick)
     def robot_specific_reset(self):
